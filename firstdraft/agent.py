d943703ff9d2a03ca8a997f341d718033db5156c
msawt <msawtell@uci.edu>
2021-08-30 17:19:27 -0700

    Fixed spacing issue

--- firstdraft/agent.py
+++ firstdraft/agent.py
@@ -70,25 +70,25 @@ def agent(observation, configuration):
         for x in range(width):
             cell = game_state.map.get_cell(x, y)
             if cell.resource != None:
-            	resource_tiles[x][y] = (cell.resource.type,cell.resource.amount)
+                resource_tiles[x][y] = (cell.resource.type,cell.resource.amount)
             
             if cell.city_tile != None:
-            	#Find city associated with CityTile
-            	city = player.cities[cell.city_tile.cityid]
-            	#Get number of turns of night that city has left before it dies
-            	turnsLeft = city.fuel//city.get_light_upkeep()
-            	city_tiles[x][y] = turnsLeft
+                #Find city associated with CityTile
+                city = player.cities[cell.city_tile.cityid]
+                #Get number of turns of night that city has left before it dies
+                turnsLeft = city.fuel//city.get_light_upkeep()
+                city_tiles[x][y] = turnsLeft
 
-            	### Put turnsLeft on tiles adjacent to all cities (for calculating tile rewards)
-            	if (x-1) >= 0:
-            		city_tiles[x-1][y] = turnsLeft
-            	elif (x+1) < width:
-            		city_tiles[x+1][y] = turnsLeft
-            	if (y-1) >= 0:
-            		city_tiles[x][y-1] = turnsLeft
-            	elif (y+1) < height:
-            		city_tiles[x][y+1] = turnsLeft
-            	###
+                ### Put turnsLeft on tiles adjacent to all cities (for calculating tile rewards)
+                if (x-1) >= 0:
+                    city_tiles[x-1][y] = turnsLeft
+                elif (x+1) < width:
+                    city_tiles[x+1][y] = turnsLeft
+                if (y-1) >= 0:
+                    city_tiles[x][y-1] = turnsLeft
+                elif (y+1) < height:
+                    city_tiles[x][y+1] = turnsLeft
+                ###
 
                 resource_fuel_value[x][y] = 0
                 resource_amount_value[x][y] = 0
@@ -160,55 +160,55 @@ def agent(observation, configuration):
     for unit in player.units:
 
         if unit.is_worker() and unit.can_act():
-        	#Iterate through every tile and find the tile with the highest reward
+            #Iterate through every tile and find the tile with the highest reward
 
 ################################################## FIND ACTION
 
-        	max_reward = -1*math.inf
-        	max_coord = None
-        	minDist = None
+            max_reward = -1*math.inf
+            max_coord = None
+            minDist = None
 
-        	for y in range(height):
-        		for x in range(width):
-        			#Calculate reward
-        			try:
-        				reward = (unit.get_cargo_capacity()*(resource_amount_value[x][y] + resource_fuel_value[x][y]) - 10*city_tiles[x][y]) / (unit.pos.distance_to(Position(x,y))+1)
-        			dist = unit.pos.distance_to(Position(x,y))
-        			if reward > max_reward:
-        				max_reward = reward
-        				max_coord = (x,y)
-        				minDist = dist
+            for y in range(height):
+                for x in range(width):
+                    #Calculate reward
+                    try:
+                        reward = (unit.get_cargo_capacity()*(resource_amount_value[x][y] + resource_fuel_value[x][y]) - 10*city_tiles[x][y]) / (unit.pos.distance_to(Position(x,y))+1)
+                    dist = unit.pos.distance_to(Position(x,y))
+                    if reward > max_reward:
+                        max_reward = reward
+                        max_coord = (x,y)
+                        minDist = dist
 
-        			elif reward == max_reward and dist < minDist: #If the rewards are the same, prioritize the one that is closer to the unit
-        				max_reward = reward
-        				max_coord = (x,y)
-        				minDist = dist
+                    elif reward == max_reward and dist < minDist: #If the rewards are the same, prioritize the one that is closer to the unit
+                        max_reward = reward
+                        max_coord = (x,y)
+                        minDist = dist
 
 
 ################################################## TAKE ACTION
-        	if max_coord != None:
-        		if unit.pos.distance_to(Position(max_coord[0],max_coord[1])) == 0: #If you're at the right cell...
-        			#Get location of all citytiles
-        			adjacentCities = [c for c in cityTiles if unit.pos.is_adjacent(c.pos)]
-        			cell = game_state.map.get_cell(x, y)
+            if max_coord != None:
+                if unit.pos.distance_to(Position(max_coord[0],max_coord[1])) == 0: #If you're at the right cell...
+                    #Get location of all citytiles
+                    adjacentCities = [c for c in cityTiles if unit.pos.is_adjacent(c.pos)]
+                    cell = game_state.map.get_cell(x, y)
 
 
-        			if (city_tiles[max_coord[0]][max_coord[1]] >= 10 or len(adjacentCities)==0) and unit.can_build() and cell.city_tile==None: #Only build a city if all adjacent cities will survive for at least 10 turns
-        				actions.append(unit.build_city())
-        			elif len(adjacentCities) > 0:																								#If they can't survive another 10 turns, transfer instead
-        				if unit.cargo.uranium > 0:
-        				    actions.append(unit.transfer(adjacentCities[0].cityid,Constants.RESOURCE_TYPES.URANIUM,unit.cargo.uranium))
-        				elif unit.cargo.coal > 0:
-        				    actions.append(unit.transfer(adjacentCities[0].cityid,Constants.RESOURCE_TYPES.COAL,unit.cargo.coal))
-        				else:
-        				    actions.append(unit.transfer(adjacentCities[0].cityid,Constants.RESOURCE_TYPES.WOOD,unit.cargo.wood))
-        			else:																														#If you can't build or transfer, do nothing
-        				actions.append(unit.move(unit.pos.direction_to(Position(max_coord[0],max_coord[1]))))
+                    if (city_tiles[max_coord[0]][max_coord[1]] >= 10 or len(adjacentCities)==0) and unit.can_build() and cell.city_tile==None: #Only build a city if all adjacent cities will survive for at least 10 turns
+                        actions.append(unit.build_city())
+                    elif len(adjacentCities) > 0:                                                                                                #If they can't survive another 10 turns, transfer instead
+                        if unit.cargo.uranium > 0:
+                            actions.append(unit.transfer(adjacentCities[0].cityid,Constants.RESOURCE_TYPES.URANIUM,unit.cargo.uranium))
+                        elif unit.cargo.coal > 0:
+                            actions.append(unit.transfer(adjacentCities[0].cityid,Constants.RESOURCE_TYPES.COAL,unit.cargo.coal))
+                        else:
+                            actions.append(unit.transfer(adjacentCities[0].cityid,Constants.RESOURCE_TYPES.WOOD,unit.cargo.wood))
+                    else:                                                                                                                        #If you can't build or transfer, do nothing
+                        actions.append(unit.move(unit.pos.direction_to(Position(max_coord[0],max_coord[1]))))
 
 
-        		else:
-        			actions.append(unit.move(unit.pos.direction_to(Position(max_coord[0],max_coord[1]))))
-        		
+                else:
+                    actions.append(unit.move(unit.pos.direction_to(Position(max_coord[0],max_coord[1]))))
+                
 
         else:
 
